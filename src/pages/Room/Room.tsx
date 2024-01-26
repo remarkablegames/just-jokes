7a4c1cbe752489cc8dbd768cafcf58ca12b357eb
Mark <mark@remarkablemark.org>
2024-01-26 18:51:26 -0500

    chore(pages): render Nickname in Room

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,4 +1,5 @@
 import { DriftDBProvider } from 'driftdb-react';
+import Nickname from 'src/components/Nickname';
 import RoomDetails from 'src/components/RoomDetails';
 import { DRIFTDB_URL } from 'src/config';
 
@@ -6,6 +7,7 @@ export default function Room() {
   return (
     <DriftDBProvider api={DRIFTDB_URL}>
       <RoomDetails />
+      <Nickname />
     </DriftDBProvider>
   );
 }
