e49e34ae99992730c7c9dc4b255a4d2fab1e91b5
Mark <mark@remarkablemark.org>
2024-01-26 23:53:18 -0500

    chore(components): add rounds to Settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -5,6 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 
+const rounds = [1, 2, 3, 4, 5];
 const players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function Settings() {
@@ -18,6 +19,22 @@ export default function Settings() {
           select
         >
           {players.map((value) => (
+            <MenuItem key={value} value={value}>
+              {value}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <br />
+        <br />
+
+        <TextField
+          defaultValue="3"
+          helperText="Number of rounds"
+          label="Rounds"
+          select
+        >
+          {rounds.map((value) => (
             <MenuItem key={value} value={value}>
               {value}
             </MenuItem>
