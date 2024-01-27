6bd7c324fb078de96889ff255f8425c12d5a8bcd
remarkablemark <remarkablemark@users.noreply.github.com>
2024-01-27 21:28:29 +0000

    Deploying to gh-pages from @ remarkablegames/just-jokes@c9531ca7d05cc43508fcd8e937f4d22e89157c54 🚀

---
+++ assets/NotFound-Y_1m8GSb.js
@@ -0,0 +1,1 @@
+import{j as o,T as r,L as t,k as n}from"./index-kXrJOpft.js";import{u as e}from"./useSetDocumentTitle-SDMxvu7Z.js";function i(){return e("Not Found"),o.jsxs(o.Fragment,{children:[o.jsx(r,{component:"h1",paragraph:!0,variant:"h4",children:"Not Found"}),o.jsxs(r,{paragraph:!0,children:["Go"," ",o.jsx(t,{component:n,to:"/",children:"home"}),"."]})]})}export{i as default};
