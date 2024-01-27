d7d1a35bb4e57cd957dfedff15117f0f66b3647b
Mark <mark@remarkablemark.org>
2024-01-27 00:05:51 -0500

    chore(components): add timer per round in Settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -7,6 +7,7 @@ import TextField from '@mui/material/TextField';
 
 const rounds = [1, 2, 3, 4, 5];
 const players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const seconds = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 export default function Settings() {
   return (
@@ -35,6 +36,22 @@ export default function Settings() {
           select
         >
           {rounds.map((value) => (
+            <MenuItem key={value} value={value}>
+              {value}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <br />
+        <br />
+
+        <TextField
+          defaultValue="30"
+          helperText="Seconds per round"
+          label="Timer"
+          select
+        >
+          {seconds.map((value) => (
             <MenuItem key={value} value={value}>
               {value}
             </MenuItem>
