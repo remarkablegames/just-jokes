dd5b5f9016079b2db65c0fd0b618b62a8cfc7961
Mark <mark@remarkablemark.org>
2024-01-27 12:45:35 -0500

    chore(components): render Players in Game

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,5 +1,9 @@
-import Round from 'src/components/Round';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import { useRound } from 'src/hooks';
+
+import Players from '../Players';
+import Round from '../Round';
 
 export default function Game() {
   const { round } = useRound();
@@ -8,5 +12,21 @@ export default function Game() {
     return null;
   }
 
-  return <Round />;
+  return (
+    <>
+      <Typography component="h1" paragraph variant="h4">
+        Round {round}
+      </Typography>
+
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Players />
+        </Grid>
+
+        <Grid item xs={8}>
+          <Round />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
--- src/components/Round/Round.tsx
+++ src/components/Round/Round.tsx
@@ -1,14 +1,9 @@
 import Typography from '@mui/material/Typography';
-import { useRound } from 'src/hooks';
 
 export default function Round() {
-  const { round } = useRound();
-
   return (
     <>
-      <Typography component="h1" paragraph variant="h4">
-        Round {round}
-      </Typography>
+      <Typography paragraph>Joke</Typography>
     </>
   );
 }
