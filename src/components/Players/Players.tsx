f65b83331de7a252466bf52f4c77461cef19a444
Mark <mark@remarkablemark.org>
2024-01-27 12:18:53 -0500

    chore(components): bold current player

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -9,24 +9,29 @@ import { useHost, usePlayer } from 'src/hooks';
 
 export default function Players() {
   const { host } = useHost();
-  const { players } = usePlayer();
+  const { players, playerId } = usePlayer();
 
   return (
     <List component={Card}>
       {Object.entries(players).map(([id, player]) => {
-        const color = player.active ? 'inherit' : 'disabled';
+        const iconColor = player.active ? 'inherit' : 'disabled';
+        const textProps =
+          playerId === id ? { sx: { fontWeight: 'bold' } } : undefined;
 
         return (
           <ListItem key={id}>
             <ListItemIcon>
               {id === host ? (
-                <HostIcon color={color} />
+                <HostIcon color={iconColor} />
               ) : (
-                <PlayerIcon color={color} />
+                <PlayerIcon color={iconColor} />
               )}
             </ListItemIcon>
 
-            <ListItemText primary={player.nickname} />
+            <ListItemText
+              primary={player.nickname}
+              primaryTypographyProps={textProps}
+            />
           </ListItem>
         );
       })}
