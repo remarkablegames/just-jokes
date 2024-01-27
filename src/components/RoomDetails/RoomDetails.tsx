14d55cf947c863b52c056676518c8fc111c1aa25
Mark <mark@remarkablemark.org>
2024-01-26 20:02:14 -0500

    chore(components): render players select in RoomDetails

--- src/components/RoomDetails/RoomDetails.tsx
+++ src/components/RoomDetails/RoomDetails.tsx
@@ -1,6 +1,14 @@
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { RoomQRCode } from 'driftdb-react';
 import { useDebugDatabase } from 'src/hooks';
+
+const players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function RoomDetails() {
   useDebugDatabase();
@@ -11,7 +19,33 @@ export default function RoomDetails() {
         Room
       </Typography>
 
-      <RoomQRCode />
+      <Card sx={{ padding: 1 }}>
+        <CardContent>
+          <TextField
+            defaultValue="4"
+            helperText="Number of players"
+            label="Players"
+            select
+          >
+            {players.map((value) => (
+              <MenuItem key={value} value={value}>
+                {value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </CardContent>
+
+        <CardActions>
+          <Button variant="contained">Start</Button>
+        </CardActions>
+      </Card>
+
+      <br />
+
+      <details>
+        <summary>QR Code</summary>
+        <RoomQRCode />
+      </details>
     </>
   );
 }
