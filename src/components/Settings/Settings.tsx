bc6213d705e62695347552f26e385e97fb5ef767
Mark <mark@remarkablemark.org>
2024-01-26 23:07:53 -0500

    refactor(components): rename RoomDetails to Settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -10,7 +10,7 @@ import { useDebugDatabase } from 'src/hooks';
 
 const players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-export default function RoomDetails() {
+export default function Settings() {
   useDebugDatabase();
 
   return (
--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,12 +1,12 @@
 import { DriftDBProvider } from 'driftdb-react';
 import Nickname from 'src/components/Nickname';
-import RoomDetails from 'src/components/RoomDetails';
+import Settings from 'src/components/Settings';
 import { DRIFTDB_URL } from 'src/config';
 
 export default function Room() {
   return (
     <DriftDBProvider api={DRIFTDB_URL}>
-      <RoomDetails />
+      <Settings />
       <Nickname />
     </DriftDBProvider>
   );
