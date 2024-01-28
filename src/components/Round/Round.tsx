a0032cb1db32a55a1e086150957195a86d666ddb
Mark <mark@remarkablemark.org>
2024-01-27 19:33:08 -0500

    chore(components): go back to lobby if there are no more rounds

--- src/components/Round/Round.tsx
+++ src/components/Round/Round.tsx
@@ -1,5 +1,6 @@
+import { useEffect } from 'react';
 import { getJoke } from 'src/helpers';
-import { useJoke, usePlayer } from 'src/hooks';
+import { useJoke, usePlayer, useRound, useSettings } from 'src/hooks';
 
 import Joke from '../Joke';
 import Vote from '../Vote';
@@ -7,6 +8,14 @@ import Vote from '../Vote';
 export default function Round() {
   const { playerId } = usePlayer();
   const { jokes } = useJoke();
+  const { round, setRound } = useRound();
+  const { settings } = useSettings();
+
+  useEffect(() => {
+    if (round > settings.rounds) {
+      setRound(0);
+    }
+  }, [round]);
 
   if (jokes[playerId]) {
     return <Vote />;
