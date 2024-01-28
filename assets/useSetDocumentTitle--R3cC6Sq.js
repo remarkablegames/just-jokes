b4d4dd5d2280c192fb4b1040762e41541e57d305
remarkablemark <remarkablemark@users.noreply.github.com>
2024-01-28 22:25:50 +0000

    Deploying to gh-pages from @ remarkablegames/just-jokes@168ec700fced01691666c2405a829d072bef7021 🚀

---
+++ assets/useSetDocumentTitle--R3cC6Sq.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-wqg5W4fp.js";function r(t){e.useEffect(()=>{document.title=t})}export{r as u};
--- assets/useSetDocumentTitle-h0QD0CxN.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-6WftGyTP.js";function r(t){e.useEffect(()=>{document.title=t})}export{r as u};
