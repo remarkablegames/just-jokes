546e6b32e3e062217080ed75283740eef1e84095
Mark <mark@remarkablemark.org>
2024-01-30 22:15:45 -0500

    perf(components): optimize with useCallback in Placeholder

--- src/components/Placeholder/Placeholder.tsx
+++ src/components/Placeholder/Placeholder.tsx
@@ -1,27 +1,39 @@
-import Autocomplete from '@mui/material/Autocomplete';
+import Autocomplete, {
+  type AutocompleteRenderInputParams,
+} from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import { useCallback } from 'react';
 import { getOptions } from 'src/helpers';
 import { playSound } from 'src/sounds';
 
 interface Props {
   category: string;
-  onChange: (event: unknown, value: string | null) => void;
+  onChange: (category: string, value: string | null) => void;
   value: string;
 }
 
 export default function Placeholder(props: Props) {
   const options = getOptions(props.category)?.map((option) => option) || [];
 
+  const onChange = useCallback(
+    (event: unknown, value: string | null) => {
+      props.onChange(props.category, value);
+    },
+    [props.category, props.onChange],
+  );
+
+  const renderInput = useCallback((params: AutocompleteRenderInputParams) => {
+    return <TextField {...params} onClick={playSound.select} />;
+  }, []);
+
   return (
     <Autocomplete
       freeSolo
       inputValue={props.value}
-      onChange={props.onChange}
-      onInputChange={props.onChange}
+      onChange={onChange}
+      onInputChange={onChange}
       options={options}
-      renderInput={(params) => (
-        <TextField {...params} onClick={playSound.select} />
-      )}
+      renderInput={renderInput}
       size="small"
     />
   );
